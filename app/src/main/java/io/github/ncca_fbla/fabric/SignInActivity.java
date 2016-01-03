953f420ee49f0f24dfdfd6604865edbd7d1072c2
James_Parsons <jamesmp98@gmail.com>
2016-01-02 21:17:22 -0500

    Worked on various Activites. I will start Parse integration as soon as I verify that the main UI works.

--- app/src/main/java/io/github/ncca_fbla/fabric/SignInActivity.java
+++ app/src/main/java/io/github/ncca_fbla/fabric/SignInActivity.java
@@ -1,13 +1,35 @@
 package io.github.ncca_fbla.fabric;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class SignInActivity extends AppCompatActivity {
+public class SignInActivity extends AppCompatActivity implements View.OnClickListener {
+    Context self;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        // Keeping a reference to the activity, because, once sign in is implemented, it will be out of scope.
+        self = this;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()) {
+            case R.id.sign_in_button_facebook:
+                // TODO: Facebook sign in.
+                break;
+
+            case R.id.sign_in_button_twitter:
+                // TODO Twitter sign in.
+                break;
+
+            default:
+                break;
+        }
     }
 }
