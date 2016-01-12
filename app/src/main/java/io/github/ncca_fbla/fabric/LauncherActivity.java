66898f646f9f59ae8be8f4e8fce74396c388fd3d
James-Parsons <jamesmp98@gmail.com>
2016-01-12 13:53:53 -0700

    Started working on some UI changes.

--- app/src/main/java/io/github/ncca_fbla/fabric/LauncherActivity.java
+++ app/src/main/java/io/github/ncca_fbla/fabric/LauncherActivity.java
@@ -3,22 +3,27 @@ package io.github.ncca_fbla.fabric;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 
 public class LauncherActivity extends Activity {
+    public Activity context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launcher);
 
+        // Store context for later use.
+        context = this;
+
         // TODO: Run the annimation.
         // But for now, we will just head to the next screen.
-        try {
-            Thread.sleep(2000);
-            startActivity(new Intent(this, SignInActivity.class));
-        } catch (InterruptedException e) {
-            Log.e("Error", e.toString());
-        }
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(context, SignInActivity.class));
+            }
+        }, 5000);
     }
 }
